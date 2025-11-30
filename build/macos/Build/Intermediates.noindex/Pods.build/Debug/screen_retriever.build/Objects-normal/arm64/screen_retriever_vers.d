6eed955cac98e5e620426ee9de11bbe5003d224e
Daniel Fokou <danieluokof@gmail.com>
2025-11-30 11:04:53 -0500

    Refactor code structure and optimize performance across multiple modules

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/screen_retriever.build/Objects-normal/arm64/screen_retriever_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/heronational/Projets/Desktop/Prompteur/build/macos/Build/Intermediates.noindex/Pods.build/Debug/screen_retriever.build/module.modulemap \
-  /Users/heronational/Projets/Desktop/Prompteur/build/macos/Build/Intermediates.noindex/Pods.build/Debug/screen_retriever.build/DerivedSources/screen_retriever_vers.c
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/window_manager.build/Objects-normal/arm64/window_manager_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/heronational/Projets/Desktop/Prompteur/build/macos/Build/Intermediates.noindex/Pods.build/Debug/window_manager.build/module.modulemap \
-  /Users/heronational/Projets/Desktop/Prompteur/build/macos/Build/Intermediates.noindex/Pods.build/Debug/window_manager.build/DerivedSources/window_manager_vers.c
